0ae7c784beaac82d836c07cc7fab94b19d0c21ef
waynellincoln <57865047+waynellincoln@users.noreply.github.com>
2020-05-08 04:44:19 -0400

    Initial Commit
    Structure and layout using Bootstrap template
    Cutting up of HTML files into includes folder

---
+++ admin/index.php
@@ -0,0 +1,29 @@
+<?php include("includes/header.php"); ?>
+
+        <!-- Navigation -->
+        
+            
+ <?php include("includes/top_nav.php"); ?>           
+            
+            
+            <!-- Sidebar Menu Items - These collapse to the responsive navigation menu on small screens -->
+        
+ <?php include("includes/side_nav.php"); ?>   
+       
+        <div id="page-wrapper">
+
+            <div class="container-fluid">
+
+                <!-- Page Heading -->
+                
+  <?php include("includes/content.php"); ?>                
+               
+                <!-- /.row -->
+
+            </div>
+            <!-- /.container-fluid -->
+
+        </div>
+        <!-- /#page-wrapper -->
+
+  <?php include("includes/footer.php"); ?>
